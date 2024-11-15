3500caca568b2bf9bdb25e7f7f7f744de9732f8f
Momin Khan <mominkhan.dev@gmail.com>
2024-11-15 13:49:54 +0500

    feat: role based auth

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,17 +1,23 @@
 import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+export const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.access_token;
 
-  if (!token) return next();
-  // if (!token) return next(new ApiResponse(401, null, "Unauthorized - no token provided !!"));
+  if (!token) return next(new ApiError(401, "Access denied. No token provided."));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(new ApiError(403, "Token is not valid."));
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded._id).select("_id role");
 
-    // decoded user by jwt
+    if (!user) {
+      return next(new ApiError(404, "User not found."));
+    }
+
     req.user = user;
-  });
-  next();
+    next();
+  } catch (error) {
+    return next(new ApiError(403, "Token is not valid."));
+  }
 };
